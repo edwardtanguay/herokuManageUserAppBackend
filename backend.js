710205047c72a40c14a9f4501de856b189d85235
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-14 23:54:27 +0100

    add heroku port

--- backend.js
+++ backend.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 const app = express();
-const port = 3016;
+const port = process.env.PORT || 3016;
 dotenv.config();
 const mongoConnectString = process.env.MONGODB_URI;
 const client = new MongoClient(mongoConnectString);
