44cee8da29d29716107f728074f48e0819766f10
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-16 00:41:15 +0100

    with env

--- backend.js
+++ backend.js
@@ -34,7 +34,7 @@ const uriIsAllowed = function (req, res, next) {
 }
 
 const allowCrossDomain = (req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+	res.header('Access-Control-Allow-Origin', process.env.ALLOWED_FRONTEND_URI);
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	res.header('Access-Control-Allow-Credentials', 'true');
