535ae8fca6145b4f9a2d22aa988d320cf9965197
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-15 21:24:16 +0100

    basic login/logout

--- backend.js
+++ backend.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongodb, { MongoClient } from 'mongodb';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = process.env.PORT || 3016;
@@ -9,6 +10,35 @@ dotenv.config();
 const mongoConnectString = process.env.MONGODB_URI;
 const client = new MongoClient(mongoConnectString);
 
+const sessionIds = [];
+let sessionId = 0;
+
+const debug = () => {
+	console.log('sessionId: ' + sessionId);
+	console.log('sessionIds: ' + sessionIds.join('|'));
+}
+
+const uriIsAllowed = function (req, res, next) {
+	const referer = req.headers.referer;
+	const host = `http://${req.headers.host}`;
+	let frontendUri = referer;
+	if (frontendUri === undefined) {
+		frontendUri = host;
+	}
+	if (frontendUri === undefined || !frontendUri.startsWith(process.env.ALLOWED_FRONTEND_URI)) {
+		res.status(403).send('access from this uri is not allowed');
+	} else {
+		next();
+	}
+}
+
+const adminIsLoggedIn = (req, res) => {
+	res.status(401).send('admin access required');
+	return false;
+}
+
+app.use(cookieParser());
+app.use(uriIsAllowed);
 app.use(cors());
 app.use(express.json());
 
@@ -18,36 +48,46 @@ const execMongo = async (done) => {
 	done(db);
 }
 
-const uriIsAllowed = (req, res) => {
-	const referer = req.headers.referer;
-	const host = `http://${req.headers.host}`;
-	let frontendUri = referer;
-	if (frontendUri === undefined) {
-		frontendUri = host;
-	}
-	if (frontendUri === undefined || !frontendUri.startsWith(process.env.ALLOWED_FRONTEND_URI)) {
-		res.status(403).send('no access');
+app.get('/login', (req, res) => {
+	if (!req.cookies.sessionId) {
+		sessionId = Math.floor(Math.random() * 100000000000);
+		sessionIds.push(sessionId);
+		debug();
+		res.cookie('sessionId', sessionId, { maxAge: 9000000000 });
+		res.json({
+			response: 'ok'
+		});
 	} else {
-		return true;
-	}
-}
-
-app.get('/', (req, res) => {
-	if (uriIsAllowed(req, res)) {
-		execMongo(async (db) => {
-			const users = await db.collection('users100').find()
-				.project({
-					name: 1,
-					username: 1,
-					email: 1
-				}).toArray();
-			res.json(users);
+		debug();
+		res.json({
+			response: 'you are already logged in'
 		});
 	}
 });
 
+app.get('/logout', (req, res) => {
+	sessionId = 0;
+	debug();
+	res.clearCookie('sessionId');
+	res.json({
+		response: 'you are now logged out'
+	});
+});
+
+app.get('/', (req, res) => {
+	execMongo(async (db) => {
+		const users = await db.collection('users100').find()
+			.project({
+				name: 1,
+				username: 1,
+				email: 1
+			}).toArray();
+		res.json(users);
+	});
+});
+
 app.delete('/deleteuser/:id', (req, res) => {
-	if (uriIsAllowed(req, res)) {
+	if (adminIsLoggedIn(req, res)) {
 		const id = req.params.id;
 		execMongo(async (db) => {
 			const deleteResult = await db.collection('users100').deleteOne({ _id: new mongodb.ObjectId(id) });
@@ -59,7 +99,7 @@ app.delete('/deleteuser/:id', (req, res) => {
 });
 
 app.post('/insertuser', (req, res) => {
-	if (uriIsAllowed(req, res)) {
+	if (adminIsLoggedIn(req, res)) {
 		const user = req.body.user;
 		execMongo(async (db) => {
 			const insertResult = await db.collection('users100').insertOne(user);
@@ -71,7 +111,7 @@ app.post('/insertuser', (req, res) => {
 });
 
 app.patch('/edituseremail/:id', (req, res) => {
-	if (uriIsAllowed(req, res)) {
+	if (adminIsLoggedIn(req, res)) {
 		const id = req.params.id;
 		const email = req.body.email;
 		res.json({
